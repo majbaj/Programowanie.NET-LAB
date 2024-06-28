4e23389ec85fc06b4bfe0a73c97bfecd335c29ee
majbaj <157068562+majbaj@users.noreply.github.com>
2024-06-28 23:27:28 +0200

    Add files via upload

---
+++ L2-Zad2/NET_L2v2_Zad2/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.IO;
+
+namespace NET_L2v2_Zad2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxOperand1 = new System.Windows.Forms.TextBox();
+            this.textBoxOperand2 = new System.Windows.Forms.TextBox();
+            this.textBoxResult = new System.Windows.Forms.TextBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonSubtract = new System.Windows.Forms.Button();
+            this.buttonMultiply = new System.Windows.Forms.Button();
+            this.buttonDivide = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxOperand1
+            // 
+            this.textBoxOperand1.Location = new System.Drawing.Point(53, 27);
+            this.textBoxOperand1.Name = "textBoxOperand1";
+            this.textBoxOperand1.Size = new System.Drawing.Size(100, 20);
+            this.textBoxOperand1.TabIndex = 0;
+            // 
+            // textBoxOperand2
+            // 
+            this.textBoxOperand2.Location = new System.Drawing.Point(53, 70);
+            this.textBoxOperand2.Name = "textBoxOperand2";
+            this.textBoxOperand2.Size = new System.Drawing.Size(100, 20);
+            this.textBoxOperand2.TabIndex = 1;
+            // 
+            // textBoxResult
+            // 
+            this.textBoxResult.Location = new System.Drawing.Point(53, 170);
+            this.textBoxResult.Name = "textBoxResult";
+            this.textBoxResult.ReadOnly = true;
+            this.textBoxResult.Size = new System.Drawing.Size(100, 20);
+            this.textBoxResult.TabIndex = 2;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(32, 119);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(23, 23);
+            this.buttonAdd.TabIndex = 3;
+            this.buttonAdd.Text = "+";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonSubtract
+            // 
+            this.buttonSubtract.Location = new System.Drawing.Point(71, 119);
+            this.buttonSubtract.Name = "buttonSubtract";
+            this.buttonSubtract.Size = new System.Drawing.Size(23, 23);
+            this.buttonSubtract.TabIndex = 4;
+            this.buttonSubtract.Text = "-";
+            this.buttonSubtract.UseVisualStyleBackColor = true;
+            this.buttonSubtract.Click += new System.EventHandler(this.buttonSubtract_Click);
+            // 
+            // buttonMultiply
+            // 
+            this.buttonMultiply.Location = new System.Drawing.Point(109, 119);
+            this.buttonMultiply.Name = "buttonMultiply";
+            this.buttonMultiply.Size = new System.Drawing.Size(23, 23);
+            this.buttonMultiply.TabIndex = 5;
+            this.buttonMultiply.Text = "*";
+            this.buttonMultiply.UseVisualStyleBackColor = true;
+            this.buttonMultiply.Click += new System.EventHandler(this.buttonMultiply_Click);
+            // 
+            // buttonDivide
+            // 
+            this.buttonDivide.Location = new System.Drawing.Point(151, 119);
+            this.buttonDivide.Name = "buttonDivide";
+            this.buttonDivide.Size = new System.Drawing.Size(23, 23);
+            this.buttonDivide.TabIndex = 6;
+            this.buttonDivide.Text = "/";
+            this.buttonDivide.UseVisualStyleBackColor = true;
+            this.buttonDivide.Click += new System.EventHandler(this.buttonDivide_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(200, 250);
+            this.Controls.Add(this.buttonDivide);
+            this.Controls.Add(this.buttonMultiply);
+            this.Controls.Add(this.buttonSubtract);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.textBoxResult);
+            this.Controls.Add(this.textBoxOperand2);
+            this.Controls.Add(this.textBoxOperand1);
+            this.Name = "Form1";
+            this.Text = "Kalkulator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxOperand1;
+        private System.Windows.Forms.TextBox textBoxOperand2;
+        private System.Windows.Forms.TextBox textBoxResult;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonSubtract;
+        private System.Windows.Forms.Button buttonMultiply;
+        private System.Windows.Forms.Button buttonDivide;
+    }
+}
---
+++ L2-Zad2/NET_L2v2_Zad2/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace NET_L2v2_Zad2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            // Tutaj inicjalizacja komponentów formularza
+
+            stopwatch.Stop();
+            TimeSpan initTime = stopwatch.Elapsed;
+
+            if (initTime.TotalSeconds > 5)
+            {
+                LogEvent("Czas uruchomienia za długi. Wynosi: " + initTime.TotalSeconds + " sekund");
+            }
+        }
+
+        private void LogEvent(string message)
+        {
+            string eventSource = "Kalkulatorek";
+            string logName = "Application";
+
+            if (!EventLog.SourceExists(eventSource))
+            {
+                EventLog.CreateEventSource(eventSource, logName);
+            }
+
+            using (EventLog eventLog = new EventLog(logName))
+            {
+                eventLog.Source = eventSource;
+                eventLog.WriteEntry(message, EventLogEntryType.Warning);
+            }
+        }
+
+        private void PerformOperation(char operation)
+        {
+            if (!double.TryParse(textBoxOperand1.Text, out double operand1) || !double.TryParse(textBoxOperand2.Text, out double operand2))
+            {
+                MessageBox.Show("Nieprawidłowe dane. Proszę podaj poprawne liczby.");
+                return;
+            }
+
+            double result = 0;
+
+            switch (operation)
+            {
+                case '+':
+                    result = operand1 + operand2;
+                    break;
+                case '-':
+                    result = operand1 - operand2;
+                    break;
+                case '*':
+                    result = operand1 * operand2;
+                    break;
+                case '/':
+                    if (operand2 == 0)
+                    {
+                        MessageBox.Show("Nie dzieli się przez zero.");
+                        return;
+                    }
+                    result = operand1 / operand2;
+                    break;
+                default:
+                    MessageBox.Show("Nieobsługiwana operacja.");
+                    break;
+            }
+
+            textBoxResult.Text = result.ToString();
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            PerformOperation('+');
+        }
+
+        private void buttonSubtract_Click(object sender, EventArgs e)
+        {
+            PerformOperation('-');
+        }
+
+        private void buttonMultiply_Click(object sender, EventArgs e)
+        {
+            PerformOperation('*');
+        }
+
+        private void buttonDivide_Click(object sender, EventArgs e)
+        {
+            PerformOperation('/');
+        }
+    }
+}
